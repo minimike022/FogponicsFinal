096e08df6e9149ac7c2169493ffd5fdbf61f25f9
minimike022 <minimike156@gmail.com>
2024-03-27 20:58:13 +0800

    Fixing Viewport

--- resources/views/Pages/User/controller.blade.php
+++ resources/views/Pages/User/controller.blade.php
@@ -10,9 +10,64 @@
 
 <body class="bg-gray-200 font-Poppins">
     <div class="flex">
+        <!-- Navigation Sidebar-->
         <div id="sidebarNav">
-            @include('Pages/Components.navigation')
+            <div class="w-[24em] h-screen relative bg-white flex justify-center left-0">
+                <div class="h-[22em] flex flex-col justify-between fixed mt-24">
+                    <div class="flex justify-center">
+                        <img src="{{asset('Images/clsuLogo.jpg')}}" alt="" class="h-[7em] w-[7em]">
+                    </div>
+                    <div class=" mt-5 flex justify-center items-center flex-col">
+                        <h1 class="text-xl">Fogponics Controller</h1>
+                        <h1 class="text-xl">(Web Based)</h1>
+                    </div>
+                    <div>
+                        <div class="flex justify-center my-10">
+                            <h1 class="font-bold text-lg">{{ Auth::user()->email}}</h1>
+                        </div>
+                        <nav class="relative">
+                            <ul class="text-xl h-[10em] flex flex-col ">
+                                <li>
+                                    <a href="{{route('dashboard')}}" class="my-4 flex">
+                                        <img src="{{asset("Images/dashboard.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1 class="text-xl">Dashboard</h1>
+                                    </a>
+                                </li>
+                                <li class="my-4">
+                                    <a href="{{route('equipments')}}" class="flex items-center">
+                                        <img src="{{asset("Images/equipment.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1>Equipments</h1>
+                                    </a>
+                                </li>
+                                <li class="my-4">
+                                    <a href="{{route("controller")}}" class="flex items-center" id="controller-parent">
+                                        <img src="{{asset("Images/controller.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1>Controller</h1>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{route("changePass")}}" class="my-4 flex items-center">
+                                        <img src="{{asset("Images/password.png")}}" alt="" class="h-[45px] w-[45px] mr-6">
+                                        <h1>Change Password</h1>
+                                    </a>
+                                </li>
+                                <form action="logout" method="POST">
+                                    @csrf
+                                    <button>
+                                        <li class="my-4 flex items-center">
+                                            <img src="{{asset("Images/logout.png")}}" alt="" class="h-[30px] w-[30px] mr-10">
+                                            <h1 class>Logout</h1>
+                                        </li>
+                                    </button>
+                                </form>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!--End of Sidebar-->
+        <!--Content Section-->
         <div class="w-full">
             <div class="w-full h-24 bg-white flex items-center">
                 <button onclick="sidebarBehavior()">
--- resources/views/Pages/User/dashboard.blade.php
+++ resources/views/Pages/User/dashboard.blade.php
@@ -10,9 +10,64 @@
 
 <body class="bg-gray-200 font-Poppins">
     <div class="flex">
-        <div id="sidebarNav" class="relative">
-            @include('Pages/Components.navigation')
+        <!-- Navigation Sidebar-->
+        <div id="sidebarNav">
+            <div class="w-[24em] h-full relative bg-white flex justify-center left-0">
+                <div class="h-[22em] flex flex-col justify-between fixed mt-24">
+                    <div class="flex justify-center">
+                        <img src="{{asset('Images/clsuLogo.jpg')}}" alt="" class="h-[7em] w-[7em]">
+                    </div>
+                    <div class=" mt-5 flex justify-center items-center flex-col">
+                        <h1 class="text-xl">Fogponics Controller</h1>
+                        <h1 class="text-xl">(Web Based)</h1>
+                    </div>
+                    <div>
+                        <div class="flex justify-center my-10">
+                            <h1 class="font-bold text-lg">{{ Auth::user()->email}}</h1>
+                        </div>
+                        <nav class="relative">
+                            <ul class="text-xl h-[10em] flex flex-col ">
+                                <li>
+                                    <a href="{{route('dashboard')}}" class="my-4 flex">
+                                        <img src="{{asset("Images/dashboard.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1 class="text-xl">Dashboard</h1>
+                                    </a>
+                                </li>
+                                <li class="my-4">
+                                    <a href="{{route('equipments')}}" class="flex items-center">
+                                        <img src="{{asset("Images/equipment.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1>Equipments</h1>
+                                    </a>
+                                </li>
+                                <li class="my-4">
+                                    <a href="{{route("controller")}}" class="flex items-center" id="controller-parent">
+                                        <img src="{{asset("Images/controller.png")}}" alt="" class="h-[40px] w-[40px] mr-7">
+                                        <h1>Controller</h1>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{route("changePass")}}" class="my-4 flex items-center">
+                                        <img src="{{asset("Images/password.png")}}" alt="" class="h-[45px] w-[45px] mr-6">
+                                        <h1>Change Password</h1>
+                                    </a>
+                                </li>
+                                <form action="logout" method="POST">
+                                    @csrf
+                                    <button>
+                                        <li class="my-4 flex items-center">
+                                            <img src="{{asset("Images/logout.png")}}" alt="" class="h-[30px] w-[30px] mr-10">
+                                            <h1 class>Logout</h1>
+                                        </li>
+                                    </button>
+                                </form>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!--End of Sidebar-->
+        <!--Content Section-->
         <div class="w-full">
             <div class="w-full h-24 bg-white flex items-center">
                 <button onclick="sidebarBehavior()">
@@ -26,10 +81,10 @@
                 </div>
                 <div class="flex justify-between mt-5 w-[90em]">
                     <div class="bg-white w-[44em] h-[30em] rounded-lg">
-                        
+
                     </div>
                     <div class="bg-white w-[44em] h-[30em] rounded-lg">
-                        
+
                     </div>
                 </div>
             </div>
